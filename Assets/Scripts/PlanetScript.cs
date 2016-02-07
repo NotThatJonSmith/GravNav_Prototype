74792baa0e39ef4d4efcac09ffed971acd80132b
levinsonalex <levinsonalex@gmail.com>
2016-02-07 15:28:50 -0500

    Added my scripts.

---
+++ Assets/Scripts/PlanetScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetScript : MonoBehaviour {
+
+    public float strengthOfAttraction = 0;
+
+    void Start () {
+
+    }
+
+    void Update()
+    {
+        Vector3 direction = transform.position - PlayerScript.S.gameObject.transform.position;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+        {
+            PlayerScript.S.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * direction);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+    public static GameObject S;
+    public List<GameObject> planets;
+
+	// Use this for initialization
+    void Awake()
+    {
+        if (S)
+        {
+            Debug.Log("Multiple players. Error.");
+            return;
+        }
+        S = gameObject;
+    }
+
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
