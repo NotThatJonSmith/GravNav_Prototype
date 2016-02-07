f5169a26601bd45dfc379b47eaf10702f5d8d90e
Jonathan Smith <jnthsmth@umich.edu>
2016-02-06 20:50:26 -0500

    Gravitation to chosen attractor objects is implemented.
    The NewtonGravityController script contains a public array that
    describes which other gameobjects this one is attracted to.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    
+    public GameObject focus;
+    public float stickSpeed;
+    private Transform startTransform;
+
+	// Use this for initialization
+	void Start () {
+        startTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void LateUpdate() {
+        startTransform.position = transform.position = focus.transform.position;
+        float horizontalRot = Time.deltaTime * stickSpeed * Input.GetAxis("RightHorizontal");
+        float verticalRot   = Time.deltaTime * stickSpeed * Input.GetAxis("RightVertical");
+        //Rigidbody focusBody = focus.GetComponent<Rigidbody>();
+        transform.eulerAngles += new Vector3(verticalRot, horizontalRot, 0);
+    }
+}
